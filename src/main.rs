61e5b5548adc945fcb1a1b4c888e39adc14b9c79
Victor Saldivia <victorsaldivia@outlook.com>
2025-07-21 16:36:25 -0400

    Se agrega los modulos de tipos de datos y control de flujo al main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // Declaración de Módulos
 mod cap01_variables;
+mod cap02_tipos_de_datos;
+mod cap03_control_de_flujo;
 
 fn main() {
-    println!("=== Rust Básico ===");
+    println!("========= Rust Básico =========");
 
     // Se llama la función principal de cada módulo
-    cap01_variables::main();
-  
+    //cap01_variables::main();
+    //cap02_tipos_de_datos::main();
+    cap03_control_de_flujo::main();
+
 }
